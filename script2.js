2127e8fd0ed0c02fe9addd7996d119ccfd52a93f
João Martins Filho <joaomfilho1995@gmail.com>
2024-03-20 13:03:22 -0300

    ajustes em import do trend

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import { Trend } from 'k6/metrics';
 import { check, sleep } from 'k6';
 import { Kubernetes } from 'k6/x/kubernetes';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
